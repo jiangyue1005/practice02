b854b39463c2e77ed0fa36a9ea952504dd56b386
y_jiang <y_jiang@192.168.1.26>
2022-05-06 18:10:41 +0900

    課題２未完成です。

--- Practice02/src/kadai2/Text.java
+++ Practice02/src/kadai2/Text.java
@@ -26,8 +26,6 @@ public class Text {
 
 		FileWriter fw;
 
-		File file3 = new File("/Users/y_jiang/Documents/kekka.txt");
-
 		FileReader fr2;
 
 		List<Omikuji> list = new ArrayList<Omikuji>();
@@ -87,44 +85,72 @@ public class Text {
 			// setLenient(false)を呼び出すことによって、このフォーマットを厳密に要求できます。
 			sdf.parse(key);
 
-			// ここはランダムの内容を呼び出したい/////////////////////////////////////////////////////////////////////
-			Random random = new Random();
-
-			Omikuji result = list.get(random.nextInt(list.size()));
-
-			System.out.println(result);
-
 			// ここはさっきの内容をファイルに書き込む//////////////////////////////////////////////////////////////
+
 			fw = new FileWriter(file2, true);
+			fr2 = new FileReader(file2);
+			BufferedReader on = new BufferedReader(fr2);
 
 			Date d = new Date();
 
-			fw.write(sdf.format(d) + "," + key + ","+ result + "\n");
-			// fw.write();
-
-			fw.close();
-
-			// ここは前のファイルを読み込んで比較////////////////////////////////////////////////////////////////////
-			fr2 = new FileReader(file3);
-			BufferedReader on = new BufferedReader(fr2);
+			fw.write(sdf.format(d) + "," + key + ",");
 
 			String date2;
+			Random random = new Random();
 			while ((date2 = on.readLine()) != null) {
 				String[] abb = date2.split(",");
 				String td = abb[0];
 				String bd = abb[1];
-				if (sdf.format(d).equals(td)) {
-					if (key.equals(bd)) {
-						System.out.println(abb[3]);
+				Omikuji re = list.get(random.nextInt(list.size()));
+				String result = re.disp() + "," + re.getNegaigoto() + "," + re.getAkinai() + "," + re.getGakumon();
+
+				if (td.equals(sdf.format(d))) {
+					if (bd.equals(key)) {
+						if (abb[2] != "") {
+							String us = abb[2] + "," + abb[3] + "," + abb[4] + "," + abb[5];
+							System.out.println(us.replace(",", "\n"));
+
+						}
 					}
-					// }else if() {
+
+				} else {
+					fw.write(result + "\n");
+					System.out.println(result.replace(",", "\n"));
 
 				}
+
+//				if (abb[0] != sdf.format(d)) {
+//
+//					fw.write(result + "\n");
+//					System.out.println(result.replace(",", "\n"));
+//				} else if ((abb[0].equals(sdf.format(d))) && abb[1] != key) {
+//
+//					fw.write(result + "\n");
+//					System.out.println(result.replace(",", "\n"));
+//
+//				} else if (td.equals(sdf.format(d))) {
+//					if (bd.equals(key)) {
+//						if (abb[2] != null) {
+//
+//							String us = abb[2] + "," + abb[3] + "," + abb[4] + "," + abb[5];
+//							System.out.println(us.replace(",", "\n"));
+//						}
+//					}
+//
+//				}else
+//					fw.write(result + "\n");
+//				System.out.println(result.replace(",", "\n"));
+
+//
+
 			}
+			fw.close();
 
 			fr2.close();
 
-		} catch (IOException e) {
+		} catch (
+
+		IOException e) {
 			System.out.println("入力された日付は存在しません。");
 
 		} catch (ParseException e) {
